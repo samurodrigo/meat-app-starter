ab4d87b119356b7668a7cd6d4554a59eff03643d
Samuel Rodrigo Ferreira <samurodrigo@gmail.com>
2017-10-24 14:37:00 -0200

    Adicionado componentes de restaurantes

---
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { RestaurantService } from "../restaurants/restaurants.service";
+import { Restaurant } from "../restaurants/restaurant/restaurant.model";
+import { ActivatedRoute } from "@angular/router";
+
+@Component({
+  selector: 'mt-restaurant-detail',
+  templateUrl: './restaurant-detail.component.html'
+})
+export class RestaurantDetailComponent implements OnInit {
+  restaurant: Restaurant
+  constructor(private restaurantService: RestaurantService, private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.restaurantService.restaurantById(this.activatedRoute.snapshot.params['id']).subscribe(restaurant => this.restaurant = restaurant);
+  }
+
+}
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -2,14 +2,23 @@ import { Restaurant } from './restaurant/restaurant.model';
 import { Http } from '@angular/http';
 import {MEAT_API} from 'app/app.api';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 import { Injectable } from '@angular/core';
+import { ErrorHandler } from "app/app.error-handler";
 
 @Injectable()
 export class RestaurantService {
     constructor(private http:Http){}
     restaurants(): Observable<Restaurant[]>{
         return this.http.get(`${MEAT_API}/restaurants`)
-                        .map(response => response.json());
+                        .map(response => response.json())
+                        .catch(ErrorHandler.handleError);
+    }
+
+    restaurantById(id: string): Observable<Restaurant>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}`)
+                 .map( response => response.json())
+                 .catch(ErrorHandler.handleError);
     }
 }
