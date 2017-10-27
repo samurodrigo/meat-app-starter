7d692bc0af073d200c9f8354d8157fac99761a53
samuel <samuel@ubuntu.ubuntu-domain>
2017-10-27 08:15:21 -0200

    commit

--- src/app/restaurant-detail/shopping-cart/cart-item.model.ts
+++ src/app/restaurant-detail/shopping-cart/cart-item.model.ts
@@ -1,7 +1,11 @@
-import { MenuItem } from "../menu-item/menu-item.model";
+import { MenuItem } from '../menu-item/menu-item.model';
 
 export class CartItem {
-    constructor(private menuItem: MenuItem, private quantity, number=1){
+    constructor(public menuItem: MenuItem, public quantity = 1){
 
+        
+    }
+    value() : number{
+        return this.menuItem.price * this.quantity;
     }
 }
--- src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
@@ -1,14 +1,29 @@
+import { CartItem } from './cart-item.model';
+import { MenuItem } from 'app/restaurant-detail/menu-item/menu-item.model';
+
 export class ShoppingCartService{
-    itens: any[];
+    itens: CartItem[] = [];
+
     clear(){
-
+        this.itens = [];
     }
 
-    total(): number{
-        return 0;
+    total(): number {
+        return this.itens
+                    .map(x => x.value())
+                    .reduce((prev, value) => prev + value, 0);
     }
 
-    addItem(item:any){}
+    addItem(menuItem: MenuItem) {
+        let foundItem = this.itens.find(x => x.menuItem.id === menuItem.id);
+        if (foundItem) {
+            foundItem.quantity = foundItem.quantity + 1;
+        } else {
+            this.itens.push(new CartItem(menuItem));
+        }
+    }
 
-    removeItem(item:any){}
+    removeItem(item: CartItem) {
+        this.itens.splice(this.itens.indexOf(item), 1);
+    }
 }
